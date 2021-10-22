e23649d46376e24218c8e514f49ad45e198b722b
LoiDoanMinh <20521554@gm.uit.edu.vn>
2021-10-22 22:07:47 +0700

    first commit

---
+++ Source/Functions/Pomodoro/Pomodoro/pomodoro/fpomodoro.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace pomodoro
+{
+    partial class fpomodoro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fpomodoro));
+            this.btnPomodoro = new System.Windows.Forms.Button();
+            this.btnShortBreak = new System.Windows.Forms.Button();
+            this.btnLongBreak = new System.Windows.Forms.Button();
+            this.lbClock = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop1 = new pomodoro.CircleButton();
+            this.btnStart1 = new pomodoro.CircleButton();
+            this.SuspendLayout();
+            // 
+            // btnPomodoro
+            // 
+            this.btnPomodoro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnPomodoro.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnPomodoro.FlatAppearance.BorderSize = 20;
+            this.btnPomodoro.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.btnPomodoro.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPomodoro.Font = new System.Drawing.Font("Elephant", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPomodoro.ForeColor = System.Drawing.Color.DarkViolet;
+            this.btnPomodoro.Location = new System.Drawing.Point(29, 73);
+            this.btnPomodoro.Name = "btnPomodoro";
+            this.btnPomodoro.Size = new System.Drawing.Size(201, 60);
+            this.btnPomodoro.TabIndex = 0;
+            this.btnPomodoro.Text = "Pomodoro";
+            this.btnPomodoro.UseVisualStyleBackColor = false;
+            this.btnPomodoro.Click += new System.EventHandler(this.btnPomodoro_Click);
+            // 
+            // btnShortBreak
+            // 
+            this.btnShortBreak.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnShortBreak.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnShortBreak.Font = new System.Drawing.Font("Elephant", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShortBreak.ForeColor = System.Drawing.Color.DarkViolet;
+            this.btnShortBreak.Location = new System.Drawing.Point(29, 158);
+            this.btnShortBreak.Name = "btnShortBreak";
+            this.btnShortBreak.Size = new System.Drawing.Size(201, 59);
+            this.btnShortBreak.TabIndex = 1;
+            this.btnShortBreak.Text = "Short Break";
+            this.btnShortBreak.UseVisualStyleBackColor = false;
+            this.btnShortBreak.Click += new System.EventHandler(this.btnShortBreak_Click);
+            // 
+            // btnLongBreak
+            // 
+            this.btnLongBreak.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnLongBreak.FlatAppearance.BorderSize = 4;
+            this.btnLongBreak.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLongBreak.Font = new System.Drawing.Font("Elephant", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLongBreak.ForeColor = System.Drawing.Color.DarkViolet;
+            this.btnLongBreak.Location = new System.Drawing.Point(29, 247);
+            this.btnLongBreak.Name = "btnLongBreak";
+            this.btnLongBreak.Size = new System.Drawing.Size(201, 57);
+            this.btnLongBreak.TabIndex = 2;
+            this.btnLongBreak.Text = "Long Break";
+            this.btnLongBreak.UseVisualStyleBackColor = false;
+            this.btnLongBreak.Click += new System.EventHandler(this.btnLongBreak_Click);
+            // 
+            // lbClock
+            // 
+            this.lbClock.BackColor = System.Drawing.Color.Turquoise;
+            this.lbClock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbClock.Font = new System.Drawing.Font("Script MT Bold", 55F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClock.ForeColor = System.Drawing.Color.Snow;
+            this.lbClock.Location = new System.Drawing.Point(261, 63);
+            this.lbClock.Name = "lbClock";
+            this.lbClock.Size = new System.Drawing.Size(266, 241);
+            this.lbClock.TabIndex = 3;
+            this.lbClock.Text = "00 : 00";
+            this.lbClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 34F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(255, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 81);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Pomodoro Timer";
+            // 
+            // btnStop1
+            // 
+            this.btnStop1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop1.BackColor = System.Drawing.Color.Firebrick;
+            this.btnStop1.FlatAppearance.BorderSize = 0;
+            this.btnStop1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop1.Font = new System.Drawing.Font("Cooper Black", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnStop1.Location = new System.Drawing.Point(558, 218);
+            this.btnStop1.Name = "btnStop1";
+            this.btnStop1.Size = new System.Drawing.Size(220, 86);
+            this.btnStop1.TabIndex = 8;
+            this.btnStop1.Text = "STOP";
+            this.btnStop1.UseVisualStyleBackColor = false;
+            this.btnStop1.Click += new System.EventHandler(this.btnStop1_Click);
+            // 
+            // btnStart1
+            // 
+            this.btnStart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart1.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnStart1.FlatAppearance.BorderSize = 0;
+            this.btnStart1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart1.Font = new System.Drawing.Font("Cooper Black", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart1.ForeColor = System.Drawing.Color.Tomato;
+            this.btnStart1.Location = new System.Drawing.Point(558, 96);
+            this.btnStart1.Name = "btnStart1";
+            this.btnStart1.Size = new System.Drawing.Size(220, 87);
+            this.btnStart1.TabIndex = 7;
+            this.btnStart1.Text = "START";
+            this.btnStart1.UseVisualStyleBackColor = false;
+            this.btnStart1.Click += new System.EventHandler(this.btnStart1_Click);
+            // 
+            // fpomodoro
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::pomodoro.Properties.Resources.Cool_wallpaper1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(812, 463);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop1);
+            this.Controls.Add(this.btnStart1);
+            this.Controls.Add(this.lbClock);
+            this.Controls.Add(this.btnLongBreak);
+            this.Controls.Add(this.btnShortBreak);
+            this.Controls.Add(this.btnPomodoro);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fpomodoro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pomodoro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPomodoro;
+        private System.Windows.Forms.Button btnShortBreak;
+        private System.Windows.Forms.Button btnLongBreak;
+        private System.Windows.Forms.Label lbClock;
+        private System.Windows.Forms.Timer timer1;
+        private CircleButton btnStart1;
+        private CircleButton btnStop1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Source/Functions/Pomodoro/Pomodoro/pomodoro/fpomodoro.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace pomodoro
+{
+    public partial class fpomodoro : Form
+    {
+        private int timeLeft;
+        public fpomodoro()
+        {
+            InitializeComponent();
+            var path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(0, 0, lbClock.Width + 1, lbClock.Height + 1);
+            this.lbClock.Region = new Region(path);
+            //btnStart1.Enabled = false;
+            //btnStop1.Enabled = false;
+        }
+
+        private void btnPomodoro_Click(object sender, EventArgs e)
+        {
+            btnStart1.Enabled = true;
+            btnStop1.Enabled = true;
+            lbClock.BackColor = Color.LightCoral;
+            this.timeLeft = 25 * 60;
+            int minutes = timeLeft / 60;
+            int second = timeLeft % 60;
+            lbClock.Text = string.Format("{0} : {1}", minutes.ToString().PadLeft(2, '0'), second.ToString().PadLeft(2, '0'));
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(timeLeft == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Your time is up!", "Thông báo");
+            }
+            else
+            {
+                timeLeft--;
+                int minutes = timeLeft / 60;
+                int second = timeLeft % 60;
+                lbClock.Text = string.Format("{0} : {1}", minutes.ToString().PadLeft(2, '0'), second.ToString().PadLeft(2, '0'));
+
+            } 
+                
+        }
+        private void btnShortBreak_Click(object sender, EventArgs e)
+        {
+            btnStart1.Enabled = true;
+            btnStop1.Enabled = true;
+            lbClock.BackColor = Color.DarkSeaGreen;
+            this.timeLeft = 5 * 60;
+            int minutes = timeLeft / 60;
+            int second = timeLeft % 60;
+            lbClock.Text = string.Format("{0} : {1}", minutes.ToString().PadLeft(2, '0'), second.ToString().PadLeft(2, '0'));
+            timer1.Stop();
+            
+        }
+
+        private void btnLongBreak_Click(object sender, EventArgs e)
+        {
+            btnStart1.Enabled = true;
+            btnStop1.Enabled = true;
+            lbClock.BackColor = Color.CadetBlue;
+            this.timeLeft = 15 * 60;
+            int minutes = timeLeft / 60;
+            int second = timeLeft % 60;
+            lbClock.Text = string.Format("{0} : {1}", minutes.ToString().PadLeft(2, '0'), second.ToString().PadLeft(2, '0'));
+            timer1.Stop();
+            
+        }
+
+        private void btnStart1_Click(object sender, EventArgs e)
+        {
+            btnStart1.Enabled = false;
+            timer1.Start();
+            btnStop1.Enabled = true;
+        }
+
+        private void btnStop1_Click(object sender, EventArgs e)
+        {
+            btnStop1.Enabled = false;
+            this.timer1.Stop();
+            btnStart1.Enabled = true;
+        }
+    }
+}

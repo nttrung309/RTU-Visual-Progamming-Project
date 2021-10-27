11d2bfcb91ac133c94ebb68f1077c421a61660f0
Phạm Nhật Minh <minhphamhklovetory@gmail.com>
2021-10-27 21:30:52 +0700

    sửa lặp câu hỏi
    câu 5,6

--- Source/Functions/MBTI/Trắc nghiệm tính cách/Đồ án 1/Cau05_06.Designer.cs
+++ Source/Functions/MBTI/Trắc nghiệm tính cách/Đồ án 1/Cau05_06.Designer.cs
@@ -63,10 +63,10 @@ namespace Đồ_án_1
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton2.Location = new System.Drawing.Point(24, 49);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(670, 44);
+            this.radioButton2.Size = new System.Drawing.Size(606, 44);
             this.radioButton2.TabIndex = 19;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Lắng nghe người khác nhiều hơn là nói.";
+            this.radioButton2.Text = "Cảm thấy thoải mái khi ở một mình.";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton1
@@ -75,10 +75,10 @@ namespace Đồ_án_1
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton1.Location = new System.Drawing.Point(24, 3);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(644, 44);
+            this.radioButton1.Size = new System.Drawing.Size(548, 44);
             this.radioButton1.TabIndex = 18;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Nói nhiều hơn là nghe người khác nói.";
+            this.radioButton1.Text = "Thích là tâm điểm của sự chú ý.";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // Next1
@@ -151,10 +151,10 @@ namespace Đồ_án_1
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton3.Location = new System.Drawing.Point(62, 3);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(338, 44);
+            this.radioButton3.Size = new System.Drawing.Size(524, 44);
             this.radioButton3.TabIndex = 20;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Chú ý các tiểu tiết.";
+            this.radioButton3.Text = "Thích những giải pháp thực tế.";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton4
@@ -163,10 +163,10 @@ namespace Đồ_án_1
             this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton4.Location = new System.Drawing.Point(62, 53);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(922, 44);
+            this.radioButton4.Size = new System.Drawing.Size(526, 44);
             this.radioButton4.TabIndex = 21;
             this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Chú ý bức tranh toàn cảnh và những việc có thể xảy ra.";
+            this.radioButton4.Text = "Thích những ý tưởng sáng tạo.";
             this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // panel1

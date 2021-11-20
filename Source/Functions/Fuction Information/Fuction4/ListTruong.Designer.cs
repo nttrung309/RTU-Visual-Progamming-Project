8b8812a747376fb876ad68e10fa0a1d82ba2714b
Phạm Nhật Minh <minhphamhklovetory@gmail.com>
2021-11-20 22:30:09 +0700

    new function

---
+++ Source/Functions/Fuction Information/Fuction4/ListTruong.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Fuction4
+{
+    partial class Truong
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TenTruong = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TenTruong
+            // 
+            this.TenTruong.AutoEllipsis = true;
+            this.TenTruong.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TenTruong.Location = new System.Drawing.Point(30, 132);
+            this.TenTruong.Name = "TenTruong";
+            this.TenTruong.Size = new System.Drawing.Size(112, 46);
+            this.TenTruong.TabIndex = 1;
+            this.TenTruong.Text = "Hello";
+            // 
+            // Truong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TenTruong);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Truong";
+            this.Size = new System.Drawing.Size(180, 187);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label TenTruong;
+    }
+}
---
+++ Source/Functions/Fuction Information/Fuction4/ListTruong.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fuction4
+{
+    public partial class Truong : UserControl
+    {
+        public Truong()
+        {
+            InitializeComponent();
+        }
+
+        public string TuaDe { get; set; }
+        public Image Icon { get; set; }
+
+    }
+}
